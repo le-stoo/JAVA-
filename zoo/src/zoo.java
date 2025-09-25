f947ca629cdbc25a726d604c380829c915658f6f
si_stoo <jouini.b.mustapha@gmail.com>
2025-09-25 18:37:13 +0100

    loula ya degla

---
+++ zoo/src/zoo.java
@@ -0,0 +1,11 @@
+package src;
+public class zoo {
+    public int nbrcage;
+    public String nom;
+
+    public zoo(int nbrcage, String nom) {
+        this.nbrcage = nbrcage;
+        this.nom = nom;
+    }
+
+}
