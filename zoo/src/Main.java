ae4bd0b3d1721175f4154c724843257e16ed3105
si_stoo <jouini.b.mustapha@gmail.com>
2025-11-03 16:26:39 +0100

    prst_2

--- zoo/src/Main.java
+++ zoo/src/Main.java
@@ -1,13 +1,45 @@
-import src.zoo;
+
 import java.util.Scanner;
+
 public class Main {
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-System.out.println("het isem e zoo");
-String nom = sc.nextLine();
-System.out.println("9adeh min 9fass fi " +nom);
-int nbrcage = sc.nextInt();
-zoo il7adi9a = new zoo(nbrcage,nom);
-System.out.println("isem il zoo :"+il7adi9a.nom+" feha :" +il7adi9a.nbrcage+" 9fass");
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter zoo name:");
+            String name = sc.nextLine();
+            
+            System.out.println("Enter zoo city:");
+            String city = sc.nextLine();
+            
+            System.out.println("Enter number of cages in " + name + ":");
+            int nbrCages = sc.nextInt();
+            sc.nextLine(); // consume newline
+            
+            Zoo zoo = new Zoo(name, city, nbrCages);
+            
+            // Create and add an animal
+            System.out.println("\nAdd an animal:");
+            System.out.println("Enter animal family:");
+            String family = sc.nextLine();
+            
+            System.out.println("Enter animal name:");
+            String animalName = sc.nextLine();
+            
+            System.out.println("Enter animal age:");
+            int age = sc.nextInt();
+            sc.nextLine(); // consume newline
+            
+            System.out.println("Is it a mammal? (true/false):");
+            boolean isMammal = sc.nextBoolean();
+            
+            Animal animal = new Animal(family, animalName, age, isMammal);
+            zoo.animals[0] = animal;
+            
+            // Display zoo information
+            System.out.println("\nZoo Information:");
+            zoo.displayZoo();
+        } finally {
+            sc.close();
+        }
+    }
 }
--- zoo/src/zoo.java
+++ zoo/src/zoo.java
@@ -1,9 +1,32 @@
-package src;
-public class zoo {
-public int nbrcage;
-public String nom;
-public zoo(int nbrcage, String nom) {
-this.nbrcage = nbrcage;
-this.nom = nom;
-}
+import java.util.Arrays;
+
+public class Zoo {
+	public Animal[] animals;
+	public String name;
+	public String city;
+	public int nbrCages;
+
+	public Zoo(String name, String city, int nbrCages) {
+		this.name = name;
+		this.city = city;
+		this.nbrCages = nbrCages;
+		this.animals = new Animal[25]; // max 25 animals
+	}
+
+	public void displayZoo() {
+		System.out.println("Zoo name: " + name);
+		System.out.println("City: " + city);
+		System.out.println("Number of cages: " + nbrCages);
+		System.out.println("Animals: " + Arrays.toString(animals));
+	}
+
+	@Override
+	public String toString() {
+		return "Zoo{" +
+				"name='" + name + '\'' +
+				", city='" + city + '\'' +
+				", nbrCages=" + nbrCages +
+				", animals=" + Arrays.toString(animals) +
+				'}';
+	}
 }
